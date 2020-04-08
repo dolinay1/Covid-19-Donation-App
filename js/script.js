450fca695b4903e1f2821b2fde4fe6bad0c89c2a
Julian Dolinay <dolinay1@gmail.com>
2020-04-07 20:26:23 -0400

    framework change

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  fetch("https://api.covid19api.com/summary", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
